cb877f8850d4c186528cdcf40112d9d8d26e6264
janelin612 <wgl1242254@gmail.com>
2017-02-16 01:38:40 +0800

    寫文件 拆method

--- devtools/src/main/java/studio/intertidal/devtools/StopWatch.java
+++ devtools/src/main/java/studio/intertidal/devtools/StopWatch.java
@@ -18,6 +18,9 @@ public class StopWatch {
         this.startMap = new HashMap<>();
     }
 
+    /**
+     * 取得Singleton的實例
+     */
     public static StopWatch getInstance() {
         if (instance == null) {
             instance = new StopWatch();
@@ -25,14 +28,36 @@ public class StopWatch {
         return instance;
     }
 
+    /**
+     * 寫入起始時間
+     *
+     * @param key 結束時用來取值的key
+     */
     public void start(String key) {
         long start = Calendar.getInstance().getTimeInMillis();
         startMap.put(key, start);
     }
 
+    /**
+     * 計時結束
+     * 並將結果印至LogCat上
+     *
+     * @param startKey 當初開始計時的時候存入的key
+     * @param log      除了時間差(ms)之外要額外顯示的log訊息
+     */
     public void end(String startKey, String log) {
-        long start = startMap.get(startKey);
-        long duration = Calendar.getInstance().getTimeInMillis() - start;
+        long duration = getDuration(startKey);
         Log.w(LOG_NAME, "" + log + "\nDuration: " + duration + " ms");
+    }
+
+    /**
+     * 取得時間差
+     *
+     * @param startKey 當初開始計時的時候存入的key
+     * @return 毫秒
+     */
+    public long getDuration(String startKey) {
+        long start = startMap.get(startKey);
+        return Calendar.getInstance().getTimeInMillis() - start;
     }
 }
